107435abb2deef2fc60070142f3271243235d5c8
cerveux <diegomartinguzman@hotmail.com.ar>
2025-05-14 23:51:20 -0300

    feat: agregar clase Socio

---
+++ Entidades/Socio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.Entidades
+{
+    internal class Socio: NoSocio
+    {
+        public DateTime vencimintoCuota { get; set; }
+        public bool carnet { get; set; }
+    }
+}
