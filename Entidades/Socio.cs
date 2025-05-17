6301bbbde0d99ff9773d5cfaca9d7f4ef5ffe6e0
guinovi <gui.novillo@gmail.com>
2025-05-17 13:03:28 -0300

    Feat: clase socio

---
+++ Entidades/Socio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.Entidades
+{
+    internal class Socio :NoSocio
+    {
+        private DateOnly VencimientoCuota;
+        private bool Carnet;
+
+
+    }
+}
